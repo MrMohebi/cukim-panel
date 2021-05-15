fbfec4ae3651fcbae4b824c473235378e0300b34
MrMohebi <MMMohebi@outlook.com>
2021-05-15 13:02:27 +0430

    add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import {BrowserRouter,Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import CreateNewRes from "./Components/CreateNewRes/CreateNewRes";
 
 function App() {
   return (
-    <></>
+    <>
+      <BrowserRouter>
+        <Route exact path='/newRes' component={CreateNewRes}/>
+      </BrowserRouter>
+    </>
   );
 }
 
